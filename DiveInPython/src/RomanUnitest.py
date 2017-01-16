ef74a18a5b51310f623704b64abc00cc8a5f10d1
griz <griz@debian-griz>
2017-01-16 23:22:13 +0200

    Added unittest chapter.

--- DiveInPython/src/RomanUnitest.py
+++ DiveInPython/src/RomanUnitest.py
@@ -1,26 +1,90 @@
-'''
-Created on 28 dec. 2016
-The Story:
+from src import roman1
+import unittest
 
-Aliens from Kepler 27b have immigrated to Earth! They have learned English and go to our stores, eat our food, dress like us, ride Ubers, use Google, etc. However, they speak English a little differently. Can you write a program that converts their Alien English to our English?
-Task:
+class KnownValues(unittest.TestCase):
+    known_values = ( (1, 'I'),
+                     (2, 'II'),
+                     (3, 'III'),
+                     (4, 'IV'),
+                     (5, 'V'),
+                     (6, 'VI'),
+                     (7, 'VII'),
+                     (8, 'VIII'),
+                     (9, 'IX'),
+                     (10, 'X'),
+                     (50, 'L'),
+                     (100, 'C'),
+                     (500, 'D'),
+                     (1000, 'M'),
+                     (31, 'XXXI'),
+                     (148, 'CXLVIII'),
+                     (294, 'CCXCIV'),
+                     (312, 'CCCXII'),
+                     (421, 'CDXXI'),
+                     (528, 'DXXVIII'),
+                     (621, 'DCXXI'),
+                     (782, 'DCCLXXXII'),
+                     (870, 'DCCCLXX'),
+                     (941, 'CMXLI'),
+                     (1043, 'MXLIII'),
+                     (1110, 'MCX'),
+                     (1226, 'MCCXXVI'),
+                     (1301, 'MCCCI'),
+                     (1485, 'MCDLXXXV'),
+                     (1509, 'MDIX'),
+                     (1607, 'MDCVII'),
+                     (1754, 'MDCCLIV'),
+                     (1832, 'MDCCCXXXII'),
+                     (1993, 'MCMXCIII'),
+                     (2074, 'MMLXXIV'),
+                     (2152, 'MMCLII'),
+                     (2212, 'MMCCXII'),
+                     (2343, 'MMCCCXLIII'),
+                     (2499, 'MMCDXCIX'),
+                     (2574, 'MMDLXXIV'),
+                     (2646, 'MMDCXLVI'),
+                     (2723, 'MMDCCXXIII'),
+                     (2892, 'MMDCCCXCII'),
+                     (2975, 'MMCMLXXV'),
+                     (3051, 'MMMLI'),
+                     (3185, 'MMMCLXXXV'),
+                     (3250, 'MMMCCL'),
+                     (3313, 'MMMCCCXIII'),
+                     (3408, 'MMMCDVIII'),
+                     (3501, 'MMMDI'),
+                     (3610, 'MMMDCX'),
+                     (3743, 'MMMDCCXLIII'),
+                     (3844, 'MMMDCCCXLIV'),
+                     (3888, 'MMMDCCCLXXXVIII'),
+                     (3940, 'MMMCMXL'),
+                     (3999, 'MMMCMXCIX'))
 
-Write a function converting their speech to ours. They tend to speak the letter a like o and o like a u. NOTE: There is an issue with this kata when it is solved in python 2x. The issue turned out to be easy to fix but there is a bug in the re-publishing system preventing the kata from being updated. The issue has been posted on Github.
+    def test_to_roman_known_values(self):
+        '''to_roman should give known result with known input'''
+        for integer, numeral in self.known_values:
+            result = roman1.to_roman(integer)
+            self.assertEqual(numeral, result)
 
-@author: radi961
-'''
-from _functools import reduce
-import re
-from src import test
-    
-def convert(st):
-    dict = {"a" : "o",
-            "o": "u"}
-    regex = re.compile("(%s)" % "|".join(map(re.escape, dict.keys())))
-    
-    
-    return regex.sub(lambda mo: dict[mo.string[mo.start():mo.end()]], st)
+    def test_to_roman_out_of_range(self):
+        '''to_roman should give known result with known input'''
+        self.assertRaises(roman1.OutOfRangeError, roman1.to_roman, 4000)
+        self.assertRaises(roman1.OutOfRangeError, roman1.to_roman, -1)
+        self.assertRaises(roman1.OutOfRangeError, roman1.to_roman, 0)
 
-if __name__ == "__main__":
-    test.assert_equals(convert('codewars'), 'cudewors')
-    #test.assert_equals(convert('hello'), 'hellu')
+    def test_to_roman_non_integers(self):
+        '''to_roman should give known result with known input'''
+        self.assertRaises(roman1.NonIntegerError, roman1.to_roman, 0.5)
+        self.assertRaises(roman1.NonIntegerError, roman1.to_roman, 1.0)
+
+
+    def test_roman_to_numeral(self):
+        for integer, numeral in self.known_values:
+            result = roman1.roman_to_numeral(numeral)
+            self.assertEqual(integer, result)
+            
+    def test_roman_to_numeral_invalid_values(self):
+        self.assertRaises(roman1.NonValidRomanValueError, roman1.roman_to_numeral, 'MMMM')
+        self.assertRaises(roman1.NonValidRomanValueError, roman1.roman_to_numeral, '')
+
+if __name__ == '__main__':
+    unittest.main()
